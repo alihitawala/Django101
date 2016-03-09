f38257dba41780b2768e16e509853317d9d54253
aliHitawala <ali.h.hita@gmail.com>
2016-03-08 21:38:33 -0600

    changes to WSGI file

--- Django101/wsgi.py
+++ Django101/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/alihitawala/my-first-blog'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django101.settings")
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 application = StaticFilesHandler(get_wsgi_application())
